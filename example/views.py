c27308b69451067fea28d9ddb464caba3bf26798
matheus costa <matheuscosta.tech@gmail.com>
2022-09-01 08:20:29 -0300

    Using forms and formsets

--- example/views.py
+++ example/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from django.forms import modelformset_factory, inlineformset_factory
+from example.forms import AuthorForm
 
 from example.models import Author, Article
 
 
 def home_view(request):
+    model_form = AuthorForm(request.POST or None)
+
     InlineFormset = inlineformset_factory(
         Author, 
         Article, 
+        form=AuthorForm,
         fields='__all__', 
         max_num=4, 
         extra=1, 
         can_delete=True
     )
-    author = Author.objects.filter().first()
 
-    model_formset = InlineFormset(request.POST or None, instance=author)
+    author = Author.objects.filter(name='author1').first()
+    formset = InlineFormset(request.POST or None, instance=author)
 
-    if model_formset.is_valid():
-        model_formset.save()
+    if formset.is_valid():
+        formset.save()
 
-    return render(request, 'example/home.html', {'formset': model_formset})
+    return render(request, 'example/home.html', {'formset': formset,
+                                                 'model_form': model_form})
