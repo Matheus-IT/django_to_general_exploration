ae28c2e711b840a2c7a8cb0d7273a074541d774b
matheus costa <matheuscosta.tech@gmail.com>
2022-11-18 10:47:10 -0300

    Alternative for returning file, not working fully

--- example/views.py
+++ example/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import StreamingHttpResponse, FileResponse
 from itertools import count
+from django.conf import settings
+from django.templatetags.static import static
+
+
 
 
 def home_view(request):
     return render(request, 'example/home.html')
 
 def download_view(request):
+    pass
     # def file_iterator(file_path, chunk_size=512):
     #     print(file_path)
     #     with open(file_path) as f:
@@ -17,11 +22,11 @@ def download_view(request):
     #             else:
     #                 break
 
-    filename = 'JS41917_598474_US_RAIMUNDA NONATA DA SILVA FRANCA_DICOM'
-    file_path = f'example/assets/{filename}.zip'
-    
+
     # response = StreamingHttpResponse(file_iterator(file_path), content_type='application/octet-stream')
-    with open(file_path) as f:
-        response = FileResponse(f)
-        response['Content-Disposition'] = f'attachment; filename={filename}.zip'
-        return response
+    
+    # This alternative worked, but the download was incomplete:
+    # file_url = static('images/example.jpg')
+    # response = FileResponse(file_url)
+    # response['Content-Disposition'] = f'attachment; filename=example.zip'
+    # return response
