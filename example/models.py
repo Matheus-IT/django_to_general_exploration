b4320cc18fc0119b9b535aced118ca89b9f3352f
matheuscosta <matheuscosta.tech@gmail.com>
2023-12-30 17:58:36 -0300

    Providing description for category

--- example/models.py
+++ example/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
 
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
