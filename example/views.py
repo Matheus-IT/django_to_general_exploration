9319c2665e38b149543055369ef7aaeb589b82a8
matheuscosta <matheuscosta.tech@gmail.com>
2023-12-30 19:29:56 -0300

    Filtering objects by age computed with annotate

--- example/views.py
+++ example/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.forms import modelformset_factory, inlineformset_factory
 from example.forms import AuthorForm
+from django.db.models import F, Func, IntegerField
+from django.db.models.functions import ExtractYear, Now
+from example.models import Author, Article, Category
+import datetime
 
-from example.models import Author, Article
+from example.utils import *
 
 
+@how_many_queries
 def home_view(request):
+    result = Author.objects.annotate(
+        age=ExtractYear(Now()) - ExtractYear('birth_date')
+    ).filter(age__gt=20)
+    print('\nfilter by age >>>', result)
+    print()
     return render(request, 'example/home.html', {})
+    
