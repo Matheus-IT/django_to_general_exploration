44bf5e7e189f791f89638e5649ef7cf6c362c1cd
matheuscosta <matheuscosta.tech@gmail.com>
2023-12-30 18:07:42 -0300

    Basic structure to start experimenting

--- example/views.py
+++ example/views.py
@@ -6,22 +6,4 @@ from example.models import Author, Article
 
 
 def home_view(request):
-    model_form = AuthorForm(request.POST or None)
-
-    InlineFormset = inlineformset_factory(
-        Author, 
-        Article, 
-        fields='__all__', 
-        max_num=4, 
-        extra=1, 
-        can_delete=True
-    )
-
-    author = Author.objects.filter(name='author1').first()
-    formset = InlineFormset(request.POST or None, instance=author)
-
-    if formset.is_valid():
-        formset.save()
-
-    return render(request, 'example/home.html', {'formset': formset,
-                                                 'model_form': model_form})
+    return render(request, 'example/home.html', {})
