cb931f41b297fa90facb3929f7042b9b534a14bb
matheuscosta <matheuscosta.tech@gmail.com>
2024-01-08 15:51:16 -0300

    Making block of code atomic within a transaction

--- example/views.py
+++ example/views.py
@@ -3,6 +3,7 @@ from django.forms import modelformset_factory, inlineformset_factory
 from example.forms import AuthorForm
 from django.db.models import F, Func, IntegerField, Q
 from django.db.models.functions import ExtractYear, Now
+from django.db.transaction import atomic
 from example.models import Author, Article, Category
 import datetime
 
@@ -17,11 +18,21 @@ def home_view(request):
     # ).filter(age__gt=20)
 
     # Using Q with and "&", and not "~"
-    result = Author.objects.filter(
-        Q(name__startswith='Larry') & ~Q(name__startswith='Robert')
-    )
-    print('\nfilter by age >>>', result)
-    print('\nsql query >>>', result.query)
+    # result = Author.objects.filter(
+    #     Q(name__startswith='Larry') & ~Q(name__startswith='Robert')
+    # )
+
+    # Making block of code atomic within a transaction
+    with atomic():
+        result = Author.objects.first()
+        result.name = 'test'
+        result.save()
+
+        result.birth_date = '2000/12/1'
+        result.save()
+
+    # print('\nresult >>>', result)
+    # print('\nsql query >>>', result.query)
 
     return render(request, 'example/home.html', {})
     
