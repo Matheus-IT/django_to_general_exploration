b8e8cbbeae229442a31b89b3706aa718039bbe14
matheuscosta <matheuscosta.tech@gmail.com>
2024-01-11 17:01:05 -0300

    Adding more profiling utilities

--- example/utils.py
+++ example/utils.py
@@ -1,5 +1,8 @@
-from django.db import connection, reset_queries
 import functools
+import time
+from django.db import connection, reset_queries
+from django.conf import settings
+from pyinstrument import Profiler
 
 
 class HowManyQueries:
@@ -33,3 +36,79 @@ def how_many_queries(func):
         print('-'*50)
         return value
     return wrapper
+
+
+class TimeElapseMeasure:
+    """Utility to be used as a context manager to calculate time execution"""
+
+    def __enter__(self):
+        self.start = time.perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.finish = time.perf_counter()
+        print(f'Time elapse {self.finish - self.start:0.4f} s')
+
+
+def time_elapse_measure(func):
+    """Utility to calculate time execution as a decorator"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        value = func(*args, **kwargs)
+        finish = time.perf_counter()
+
+        print(f'Time elapse: {finish - start:0.4f} s')
+        return value
+    return wrapper
+
+
+class SampleTimeExpensiveCalls:
+    """
+    Utility to sample most time consuming calls as a context manager
+    ATTENTION: Doesn't work well with multiple threads
+    """
+
+    def __init__(self, open_in_browser=False):
+        self.output_html = open_in_browser
+
+    def __enter__(self):
+        self.profiler = Profiler()
+        self.profiler.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.profiler.stop()
+        if self.output_html:
+            output = self.profiler.output_html()
+            with open(f"{settings.BASE_DIR}/profiler_output.html", "w") as f:
+                f.write(output)
+        else:
+            self.profiler.print(color=True)
+
+
+def sample_time_expensive_calls(output_html=False):
+    """
+    Utility to sample most time consuming calls as a decorator
+    ATTENTION: Doesn't work well with multiple threads
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            profiler = Profiler()
+            profiler.start()
+
+            result = func(*args, **kwargs)
+
+            profiler.stop()
+
+            if not output_html:
+                profiler.print(color=True)
+                return result
+
+            output = profiler.output_html()
+            with open(f"{settings.BASE_DIR}/profiler_output.html", "w") as f:
+                f.write(output)
+            return result
+        return wrapper
+    return decorator
